d799aa5f60307ca84b06aae349808711b26369c0
mirtaher <mmirtaher@gmail.com>
2016-07-16 20:10:04 -0500

    Make it a function of i and r

--- R/E.1.u.d.der.R
+++ R/E.1.u.d.der.R
@@ -3,8 +3,8 @@
 #' This function simulate the income process for two spaouses
 #' @export
 
-E.1.u.d.der <-function(S, type = c("u", "U"), spouse = c("h", "w")){
-  
+E.1.u.d.der <-function(S, type = c("u", "U"), spouse = c("h", "w"), i, r){
+
   # Distributing parameters
   par <- param()
   N <- par$N
@@ -16,44 +16,47 @@ E.1.u.d.der <-function(S, type = c("u", "U"), spouse = c("h", "w")){
   beta <- par$beta
   ybar <- par$ybar
   delta <- par$delta
-  
-  
+
+
   wt.h <- par$wt.h
   wt.w <- par$wt.w
   u <- par$u
   U <- par$U
   u.grad <- par$u.grad
-  
+
   # Cartesian product
   index <- expand.grid(1:N, 1:reps1)
   index2 <- expand.grid(1:N, 1:reps1, 1:reps2)
-  
+
   c2.d.array <- function(S){
     res <- 1/beta * delta * S + y2
     return(res)
   }
-  
+
   if (!missing(spouse)){
     if (type == "u"){
       if (spouse == "h"){
-        U.2.m.vec <- mapply(function(h) u.grad(h) * delta * 1/beta , c2.d.array(S)[,1,,])
+        U.2.m.vec <- mapply(function(h) u.grad(h) * delta * 1/beta ,
+                            matrix(period.2.d(S, i, r, 1:reps2), ncol = 2)[,1])
       }
       if (spouse == "w"){
-        U.2.m.vec <- mapply(function(w) u.grad(w) * (1-delta) * 1/beta, c2.d.array(S)[,2,,])
+        U.2.m.vec <- mapply(function(w) u.grad(w) * (1-delta) * 1/beta,
+                            matrix(period.2.d(S, i, r, 1:reps2), ncol = 2)[,2])
       }
     }
-    
+
     if (type == "U"){
-      U.2.m.vec <- mapply(function(h,w) wt.h * u.grad(h) * delta * 1/beta + wt.w * u.grad(w) * (1-delta) * 1/beta, 
-                          c2.d.array(S)[,1,,], c2.d.array(S)[,2,,])
+      U.2.m.vec <- mapply(function(h,w) wt.h * u.grad(h) * delta * 1/beta + wt.w * u.grad(w) * (1-delta) * 1/beta,
+                          matrix(period.2.d(S, i, r, 1:reps2), ncol = 2)[,1],
+                          matrix(period.2.d(S, i, r, 1:reps2), ncol = 2)[,2])
     }
   } else {
-    U.2.m.vec <- mapply(function(h,w) wt.h * u.grad(h) * delta * 1/beta + wt.w * u.grad(w) * (1-delta) * 1/beta, 
-                        c2.d.array(S)[,1,,], c2.d.array(S)[,2,,])
+    U.2.m.vec <- mapply(function(h,w) wt.h * u.grad(h) * delta * 1/beta + wt.w * u.grad(w) * (1-delta) * 1/beta,
+                        matrix(period.2.d(S, i, r, 1:reps2), ncol = 2)[,1],
+                        matrix(period.2.d(S, i, r, 1:reps2), ncol = 2)[,2])
   }
-  
-  
-  U.2.m <- array(U.2.m.vec, dim = c(N,reps1, reps2))
-  res <- apply(U.2.m, MARGIN = c(1,2), FUN = mean)
+
+
+  res <- mean(U.2.m.vec)
   return(res)
 }
