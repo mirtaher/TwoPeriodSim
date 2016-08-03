1e6c9bce47302769063242524865d38a183eb068
mirtaher <mmirtaher@gmail.com>
2016-08-03 12:12:24 -0500

    Handle missing values in income. Polishing the initial values

--- R/period.1.d.R
+++ R/period.1.d.R
@@ -37,45 +37,49 @@ period.1.d <- function(i, r){
   index <- expand.grid(1:N, 1:reps1)
   index2 <- expand.grid(1:N, 1:reps1, 1:reps2)
 
-  eval_f_1_d <- function(c, i, r){
-    return(list("objective" = - U(c[1], c[2])- beta * E.1.u.d(c[3], type = "U", i = i, r = r),
-                "gradient" = c(-wt.h * u.grad(c[1]),
-                               -wt.w * u.grad(c[2]),
-                               (-beta) * E.1.u.d.der(c[3], type = "U", i = i, r = r) )))
+  miss <- is.na(y1[i,1,r]) | is.na(y1[i,2,r])
+  if (miss){
+    res <- rep(NA, 3)
+    return(res)
+  } else {
+    eval_f_1_d <- function(c, i, r){
+      return(list("objective" = - U(c[1], c[2])- beta * E.1.u.d(c[3], type = "U", i = i, r = r),
+                  "gradient" = c(-wt.h * u.grad(c[1]),
+                                 -wt.w * u.grad(c[2]),
+                                 (-beta) * E.1.u.d.der(c[3], type = "U", i = i, r = r) )))
+    }
+
+    eval_g_eq_1 <- function(c, i, r){
+      constr <- c( c[1] + c[2] + c[3] - y1[i,1,r] - y1[i,2,r])
+      grad <-  c(1, 1, 1)
+      return( list( "constraints"=constr, "jacobian"=grad ) )
+    }
+
+    local_opts <- list( "algorithm" = "NLOPT_LD_MMA",
+                        "xtol_rel"  = 1.0e-7)
+    opts <- list( "algorithm" = "NLOPT_LD_SLSQP",
+                  "xtol_rel"  = 1.0e-7,
+                  "maxeval"   = 10000,
+                  "local_opts" = local_opts )
+
+    ini <- borrow.const(i, r)
+    S.lower <- ini$lower
+    S.upper <- ini$upper
+
+    c.upper <- y1[i,1,r] + y1[i,2,r] - S.lower
+    c.lower <- 0
+
+    ub <- c(c.upper, c.upper, S.upper)
+    lb <- c(c.lower, c.lower, S.lower)
+
+    S0 <- (S.lower + S.upper)/2
+    c0 <- (y1[i,1,r] + y1[i,2,r] - S0)/2
+    x0 <- c(c0, c0, S0)
+
+    res <- nloptr(x0 = x0, eval_f = eval_f_1_d, lb = lb, ub = ub,
+                  eval_g_eq = eval_g_eq_1, opts = opts, i = i, r = r)
+    return(res$solution)
   }
 
-  eval_g_eq_1 <- function(c, i, r){
-    constr <- c( c[1] + c[2] + c[3] - y1[i,1,r] - y1[i,2,r])
-    grad <-  c(1, 1, 1)
-    return( list( "constraints"=constr, "jacobian"=grad ) )
-  }
 
-  local_opts <- list( "algorithm" = "NLOPT_LD_MMA",
-                      "xtol_rel"  = 1.0e-7)
-  opts <- list( "algorithm" = "NLOPT_LD_SLSQP",
-                "xtol_rel"  = 1.0e-7,
-                "maxeval"   = 10000,
-                "local_opts" = local_opts )
-
-  equal <- (y1[i,1,r] + y1[i,2,r])/2
-  if (equal < 0) {
-    y1.expand <- array(rep(y1,reps2), dim = c(N,2,reps1, reps2))
-    equal.neg.expand <- (y1.expand[i,1,r,] + y1.expand[i,2,r,] + y2[i,1,r,] + y2[i,2,r,])/4
-    equal.neg <- mean(equal.neg.expand)
-    c0 <- c(equal.neg, equal.neg, 0)
-  }
-  else{
-    c0 <- c(equal, equal, 0)
-  }
-
-  ini <- borrow.const(i, r)
-  S.lower <- ini$lower
-  S.upper <- ini$upper
-
-  ub <- c(equal*2, equal*2, S.upper)
-  lb <- c(0, 0, S.lower)
-
-  res <- nloptr(x0 = c0, eval_f = eval_f_1_d, lb = lb, ub = ub,
-                eval_g_eq = eval_g_eq_1, opts = opts, i = i, r = r)
-  return(res$solution)
 }
