e148290bc19eed2757200da0b4b2235d9d840e65
mirtaher <mmirtaher@gmail.com>
2016-08-10 09:46:55 -0500

    Make it a function of income process shocks and gaurd against NAs in income

--- R/period.1.d.R
+++ R/period.1.d.R
@@ -3,7 +3,7 @@
 #' This function simulate the income process for two spaouses
 #' @export
 
-period.1.d <- function(i, r){
+period.1.d <- function(i, r, sigma_eta_h = param()$sigma_eta_h, rho = param()$rho, phi = param()$phi){
   library(nloptr)
 
   # Distributing parameters
@@ -25,61 +25,60 @@ period.1.d <- function(i, r){
   U <- par$U
   u.grad <- par$u.grad
 
-  sigma_eta_h <- par$sigma_eta_h
-  rho <- par$rho
-  phi <- par$phi
-
-  income <- TwoPeriodSim::incomeProcess(sigma_eta_h = sigma_eta_h, Rho = rho, Phi = phi)
-  y1 <- income$y1
-  y2 <- income$y2
-
   # Cartesian product
   index <- expand.grid(1:N, 1:reps1)
   index2 <- expand.grid(1:N, 1:reps1, 1:reps2)
 
-  miss <- is.na(y1[i,1,r]) | is.na(y1[i,2,r])
-  if (miss){
+  income <- TwoPeriodSim::incomeProcess(sigma_eta_h = sigma_eta_h, Rho = rho, Phi = phi)
+  if (!income$cov.det) {
     res <- rep(NA, 3)
     return(res)
-  } else {
-    eval_f_1_d <- function(c, i, r){
-      return(list("objective" = - U(c[1], c[2])- beta * E.1.u.d(c[3], type = "U", i = i, r = r),
-                  "gradient" = c(-wt.h * u.grad(c[1]),
-                                 -wt.w * u.grad(c[2]),
-                                 (-beta) * E.1.u.d.der(c[3], type = "U", i = i, r = r) )))
+  }else {
+    y1 <- income$y1
+    y2 <- income$y2
+
+    miss <- is.na(y1[i,1,r]) | is.na(y1[i,2,r])
+    if (miss){
+      res <- rep(NA, 3)
+      return(res)
+    } else {
+      eval_f_1_d <- function(c, i, r){
+        return(list("objective" = - U(c[1], c[2])- beta * E.1.u.d(c[3], type = "U", i = i, r = r, sigma_eta_h = sigma_eta_h, rho = rho, phi = phi),
+                    "gradient" = c(-wt.h * u.grad(c[1]),
+                                   -wt.w * u.grad(c[2]),
+                                   (-beta) * E.1.u.d.der(c[3], type = "U", i = i, r = r, sigma_eta_h = sigma_eta_h, rho = rho, phi = phi) )))
+      }
+
+      eval_g_eq_1 <- function(c, i, r){
+        constr <- c( c[1] + c[2] + c[3] - y1[i,1,r] - y1[i,2,r])
+        grad <-  c(1, 1, 1)
+        return( list( "constraints"=constr, "jacobian"=grad ) )
+      }
+
+      local_opts <- list( "algorithm" = "NLOPT_LD_MMA",
+                          "xtol_rel"  = 1.0e-7)
+      opts <- list( "algorithm" = "NLOPT_LD_SLSQP",
+                    "xtol_rel"  = 1.0e-7,
+                    "maxeval"   = 10000,
+                    "local_opts" = local_opts )
+
+      ini <- borrow.const(i, r, sigma_eta_h = sigma_eta_h, rho = rho, phi = phi)
+      S.lower <- ini$lower
+      S.upper <- ini$upper
+
+      c.upper <- y1[i,1,r] + y1[i,2,r] - S.lower
+      c.lower <- 0
+
+      ub <- c(c.upper, c.upper, S.upper)
+      lb <- c(c.lower, c.lower, S.lower)
+
+      S0 <- (S.lower + S.upper)/2
+      c0 <- (y1[i,1,r] + y1[i,2,r] - S0)/2
+      x0 <- c(c0, c0, S0)
+
+      res <- nloptr(x0 = x0, eval_f = eval_f_1_d, lb = lb, ub = ub,
+                    eval_g_eq = eval_g_eq_1, opts = opts, i = i, r = r)
+      return(res$solution)
     }
-
-    eval_g_eq_1 <- function(c, i, r){
-      constr <- c( c[1] + c[2] + c[3] - y1[i,1,r] - y1[i,2,r])
-      grad <-  c(1, 1, 1)
-      return( list( "constraints"=constr, "jacobian"=grad ) )
-    }
-
-    local_opts <- list( "algorithm" = "NLOPT_LD_MMA",
-                        "xtol_rel"  = 1.0e-7)
-    opts <- list( "algorithm" = "NLOPT_LD_SLSQP",
-                  "xtol_rel"  = 1.0e-7,
-                  "maxeval"   = 10000,
-                  "local_opts" = local_opts )
-
-    ini <- borrow.const(i, r)
-    S.lower <- ini$lower
-    S.upper <- ini$upper
-
-    c.upper <- y1[i,1,r] + y1[i,2,r] - S.lower
-    c.lower <- 0
-
-    ub <- c(c.upper, c.upper, S.upper)
-    lb <- c(c.lower, c.lower, S.lower)
-
-    S0 <- (S.lower + S.upper)/2
-    c0 <- (y1[i,1,r] + y1[i,2,r] - S0)/2
-    x0 <- c(c0, c0, S0)
-
-    res <- nloptr(x0 = x0, eval_f = eval_f_1_d, lb = lb, ub = ub,
-                  eval_g_eq = eval_g_eq_1, opts = opts, i = i, r = r)
-    return(res$solution)
   }
-
-
 }
